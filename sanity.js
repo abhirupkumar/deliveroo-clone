246ca0c29b94238360b8002d08550343c2ab0429
Abhirup Kumar Bhowmick <94789673+abhirupkumar@users.noreply.github.com>
2022-11-24 09:39:05 +0530

    Updated Sanity.js

--- sanity.js
+++ sanity.js
@@ -1,9 +1,10 @@
 import sanityClient from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
+import { SANITY_PROJECT_ID, SANITY_DATA_SET } from '@env'
 
 const client = sanityClient({
-    projectId: 'r3bsf8hy',
-    dataset: 'production',
+    projectId: SANITY_PROJECT_ID,
+    dataset: SANITY_DATA_SET,
     useCdn: true,
     apiVersion: '2021-10-21'
 })
